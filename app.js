699c7fff86bd1b6943742ed476c30b9b57910c88
GrzegorzD <gdziemidokxxiii@gmail.com>
2016-11-17 16:08:12 +0100

    Pawie gotowy projekt

---
+++ app.js
@@ -0,0 +1,175 @@
+
+/*rozwiniecie kółek na wstępie*/
+$(function(){
+
+	var cv = $('.cv');
+	var skills = $('.skills');
+	var portfolio = $('.portfolio');
+	var favour = $('.favour');
+	var width = cv.width();
+	var height = cv.height();
+	var text = $('.text');
+	var widthtext = text.width();
+	var heighttext = text.height();
+
+
+	
+
+
+cv.delay(500).animate({width: 5*width+'px', height: 5*height+'px'},2000);
+cv.animate({left:'-=100px'},1000);
+
+skills.delay(600).animate({width: 4*width, height: 4*height},3000);
+skills.animate({left:'-=50px'},1000);
+
+portfolio.delay(400).animate({width: 3*width, height: 3*height},4000);
+portfolio.animate({left:'-=30px'},1000);
+
+favour.delay(300).animate({width: 2*width, height: 2*height},5000);
+
+
+
+
+});
+
+/* pokazanie logotypów technologi*/
+$(function(){
+
+	
+	var skills  = $('.skills');
+	var textskil = $('.content2');
+	var obrazek = $('.obrazek');
+
+	
+		skills.on('click',function(){
+			console.log(obrazek);
+
+			obrazek.animate({top:'-=200px'},1500);
+
+				obrazek.fadeOut(5000, function(){
+
+				$(this).animate({top:'+=200px'},1000).fadeIn(1000)
+			});
+		});
+	});
+
+/*spadnięcie eszystkich planet do dołu okna przeglądarki*/
+$(function(){
+
+	var animcss=$('.animcss')
+	var name = $('.name');
+	var width = name.width();
+	var height = name.height();
+	var portfolio = $('.portfolio');
+	var skills  = $('.skills');
+	var cv = $('.cv');
+	var favour = $('.favour');
+	favour.on('click',function(){
+		favour.animate({ top: '+=300px' }, 4000, 'easeOutBounce');
+	cv.animate({ top: '+=150px' }, 1000, 'easeOutBounce');
+	portfolio.animate({ top: '+=250px' }, 2000, 'easeOutBounce');
+	skills.animate({ top: '+=200px' }, 3000, 'easeOutBounce');
+	name.animate({width: 1.3*width +'px', height: 1.3*height +'px'},1000);
+	name.animate({ top: '+=400px' }, 2000, 'easeOutBounce',function(){
+		name.animate({left:'+=5px'},500);
+	cv.addClass('animcss');	
+	cv.animate({left:'+=50px'},800,function(){
+		skills.addClass('animcss'),
+	skills.animate({left:'+=50px'}),
+	portfolio.addClass('animcss'),
+	portfolio.animate({left:'+=30px'},800,function(){
+		favour.animate({left:'+=800px'},500);
+		favour.animate({left:'-=800px'},500);
+		favour.animate({top:'-=149px', left:'-=120px'},300);
+		favour.animate({top:'-=450px'},500);
+	favour.animate({top:'+=450px'},300,function(){
+		portfolio.animate({top:'+=150px'},300, function(){
+	favour.animate({top:'-=450px'},500);	
+	favour.animate({top:'+=450px',left:'-=100px'},300,function(){
+		skills.animate({top:'+=200px',left:'-=50px'},300, function(){
+			favour.animate({top:'-=450px'},500);	
+	favour.animate({top:'+=430px',left:'-=250px'},300,function(){
+		cv.animate({top:'+=250px'},300, function(){
+			favour.animate({top:'-=500px'},500);
+	favour.animate({top:'+=780px',left:'-=280px'},500, function(){
+		name.animate({ top: '-=400px' },1000);	
+	name.animate({ top: '+=200px',left:'+=550px' },1000, function(){
+		name.animate({width: 2*width +'px', height: 2*height +'px'},1000,function(){
+			name.toggle( "explode" );
+	});	
+
+
+	});	
+	});	
+
+	});
+
+	});	
+	});	
+
+	});
+		
+
+	});	
+
+	});
+		
+	});
+	
+	
+
+	  });
+	
+
+	});
+
+});	
+
+});
+	/*uruchomienie animacji star wars za pomocą click oraz muzyki*/
+
+	$(function(){
+
+		var cv = $('.cv');
+		var scroll = $('.scroll');
+		var content1 = $('.content1');
+		var audio = document.querySelector('audio');
+
+		cv.on('click',function(){
+
+		audio.play();
+
+		content1.addClass('scroll');
+
+			});
+
+	});
+
+// zamiana klass w portfolio
+
+	$(function(){
+
+		var portfolio = $('.portfolio');
+		var portfolioshow =$('.portfolioshow')
+		var text1 = $('.text1');
+		portfolio.on('click',function(){
+			portfolio.removeClass('portfolio');
+			portfolio.addClass('portfolioshow');
+			text1.text( 'Prace na githubie');
+
+		});
+
+    		
+	});				
+		
+    		
+			
+
+
+
+
+
+
+
+
+
